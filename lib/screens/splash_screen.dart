dcd0e34a67e12921fca3ef6a1de065f3f3240ff8
domitrov <justinomphithetse@gmail.com>
2025-03-23 01:53:29 +0200

    modifying splash_screen.dart and updated ndk version in build.gradle.kts

--- lib/screens/splash_screen.dart
+++ lib/screens/splash_screen.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
+import 'package:animated_splash_screen/animated_splash_screen.dart';
+import '../screens/home_page.dart';
 
 class SplashScreen extends StatefulWidget {
   const SplashScreen({super.key});
@@ -13,13 +15,19 @@ class _SplashScreenState extends State<SplashScreen> {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.black,
-      body: Center(
-        child: Text('muted.', style: GoogleFonts.notoSans(
-            fontSize: 50
-            ,
+      body: AnimatedSplashScreen(
+        duration: 3000,
+        splash: Text(
+          'muted.',
+          style: GoogleFonts.notoSans(
+            fontSize: 50,
             fontWeight: FontWeight.w900,
-            color: Colors.white
-          ),),
+            color: Colors.white,
+          ),
+        ),
+        nextScreen: HomePage(),
+        splashTransition: SplashTransition.scaleTransition
+        backgroundColor: Colors.black,
       ),
     );
   }
